47c6f9d0d6f935f963553de8b0df401e3926afc8
Lucas Laguilio <laguiliolucas@gmail.com>
2025-12-10 10:49:21 -0400

    Fix em tudo

--- src/rotas/rotas-autenticadas.ts
+++ src/rotas/rotas-autenticadas.ts
@@ -16,11 +16,10 @@ rotas.post('/adicionarItem', Auth, carrinhoController.adicionarItem)
 rotas.post('/removerunidadeItem', Auth, carrinhoController.removerunidadeItem)
 rotas.get('/carrinho', Auth, carrinhoController.listar)
 rotas.delete('/carrinho', Auth, carrinhoController.remover)
-
-
 rotas.post("/removerItem", carrinhoController.removerItem);
 rotas.get("/carrinho/:usuarioId", carrinhoController.listar);
 rotas.delete("/carrinho/:usuarioId", carrinhoController.remover);
+rotas.post('/criar-pagamento-cartao', Auth, carrinhoController.criarPagamento);
 
 
 rotas.delete("/produtos/excluir/:id", Auth, AuthAdmin, produtosController.excluir)
